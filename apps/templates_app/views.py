20eb5868ee821f97d7f4912ddf4fca3e5d28e32b
rasul312 <freeman190793@gmail.com>
2021-02-24 15:29:11 +0600

    rebase st yle

--- apps/templates_app/views.py
+++ apps/templates_app/views.py
@@ -2,7 +2,7 @@ from django.views.generic import TemplateView
 
 
 class IndexView(TemplateView):
-    template_name = 'pages/index/index.html'
+    template_name = 'pages/index.html'
 
 
 class ExampleView(TemplateView):
